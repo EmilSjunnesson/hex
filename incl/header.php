44b5931c0eb678493db01a322854daea9961b37c
Emil Sjunnesson <byrhult@gmail.com>
2015-01-06 10:40:31 +0100

    added favicon

--- incl/header.php
+++ incl/header.php
@@ -1,1 +1,13 @@
-<?php
+<!doctype html>
+<html lang='en' class='no-js'>
+<head>
+<meta charset='utf-8' />
+<title><?=isset($title) ? $title . ' | JavaScript' : 'Template for testprograms in JavaScript'?></title>
+<link rel="icon" type="image/png" href="../img/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="../img/favicon-32x32.png" sizes="32x32">
+<link rel="stylesheet/less" type="text/css" href="style.less">
+<script src="../js/less.min.js"></script>
+<script src="../js/modernizr.js"></script>
+</head>
+<body>
+<div id="wrapper">
--- start/index.php
+++ start/index.php
@@ -1,1 +1,3 @@
-<?php
+<?php $title='Template for testprogram'; include(__DIR__ . '/../incl/header.php'); ?>
+
+<?php $path=__DIR__; include(__DIR__ . '/../incl/footer.php'); ?>
