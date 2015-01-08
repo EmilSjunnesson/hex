3b219cea38d2350463a957af169198a807b89b27
Emil Sjunnesson <byrhult@gmail.com>
2015-01-08 18:18:08 +0100

    basic mute/unmute

--- game/index.php
+++ game/index.php
@@ -1,4 +1,11 @@
 <?php $title='Game'; include(__DIR__ . '/../incl/header.php'); ?>
+<input type="checkbox" name="un-mute" id="un-mute">
+<label for="un-mute" class="unmute">
+	<img src="http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg" alt="Mute_Icon.svg" title="Mute icon">
+</label>
+<label for="un-mute" class="mute">
+	<img src="http://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg" alt="Speaker_Icon.svg" title="Unmute/speaker icon">
+</label>
 <div id="board">
 <div id="cards">
 <?php for ($i = 0; $i <12; $i++) : ?>
