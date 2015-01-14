e92ce689062da64bab4274acd4409af9b30c2ee7
Emil Sjunnesson <byrhult@gmail.com>
2015-01-14 19:40:17 +0100

    Almost finnished with show score

--- score/index.php
+++ score/index.php
@@ -1,8 +1,19 @@
-<?php $title='Template for testprogram'; include(__DIR__ . '/../incl/header.php'); 
+<?php $title='Score'; include(__DIR__ . '/../incl/header.php'); 
 if ((!isset($_POST['score'])) && (!isset($_POST['timeBonus']))) {
 	die('You need to win the game to access this page');
 }?>
-<p class="hidden" id="score"><?=$_POST['score']?></p>
-<p class="hidden" id="timeBonus"><?=$_POST['timeBonus']?></p>
+
+<div id="show-score">
+	<img alt="hex" src="../img/hex.png">
+	
+	<p>Score:<br>
+	<span id="score"><?=$_POST['score']?></span></p>
+	
+	<p>Time bonus:<br>
+	<span id="timeBonus"><?=$_POST['timeBonus']?></p>
+	
+	<p>Final score:<br>
+	<span id="finalScore">0</p>
+</div>
 
 <?php $path=__DIR__; include(__DIR__ . '/../incl/footer.php'); ?>
--- score/main.js
+++ score/main.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
-   var score, timeBouns;
+   var score, timeBounsSec, timeBonusMin, finalScore,
+   minutes, seconds;
    
    // Get score and timeBonus from the game
    score = $('#score').text();
-   timeBouns = $('#timeBonus').text();
-   console.log('score: ' + score + ' timeBonus: '+ timeBouns);
+   timeBounsSec = $('#timeBonus').text();
+   console.log('score: ' + score + ' timeBonus: '+ timeBounsSec);
+
+   timeBonusMin = roundToDecimals(timeBounsSec/60, 2);
+   finalScore = calculateFinalScore(score, timeBonusMin);
+   
+   minutes = Math.floor(timeBounsSec / 60);
+   seconds = timeBounsSec - minutes * 60;
+   
+   // Print out score
+   $('#timeBonus').text(minutes + ' min ' + seconds + ' sec');
+   $('#finalScore').text(finalScore);
+   
+   //TODO kolla om nytt highscore annars alternativ spela igen / till menu
 });
+
+//get final score from time and score
+function calculateFinalScore(s, tb) {
+	var fs = 0;
+	fs = (s/tb*10)+parseInt(s); // DETTA VISA SOM TIMEBONUS?
+	return parseInt(fs);
+}
+
+// Round to decimals
+function roundToDecimals(d, c) {
+	var temp = parseInt(d * Math.pow(10, c));
+	return (parseFloat(temp) / Math.pow(10, c));
+}
