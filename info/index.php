9b8763ed161d2748f2bb9e944c2658bea06d8f48
Emil Sjunnesson <byrhult@gmail.com>
2015-01-18 12:59:15 +0100

    Began about hex

--- info/index.php
+++ info/index.php
@@ -1,3 +1,11 @@
 <?php $title='Template for testprogram'; include(__DIR__ . '/../incl/header.php'); ?>
 
+<div id="info">
+	<a id="back" href="../start">&#10094; Back to main menu</a>
+	<header>
+		<img alt="hex" src="../img/hex.png">
+	</header>
+	<h1>Om Hex</h1>
+</div>
+
 <?php $path=__DIR__; include(__DIR__ . '/../incl/footer.php'); ?>
